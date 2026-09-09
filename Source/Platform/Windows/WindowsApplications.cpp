dc8dc54ae95dd7b27eb45c22fe2054d04a758880
xt9y <felix@xt9y.de>
2026-09-10 00:50:37 +0200

    Fix Windows property key include order

--- Source/Platform/Windows/WindowsApplications.cpp
+++ Source/Platform/Windows/WindowsApplications.cpp
@@ -1,7 +1,13 @@
 #include "WindowsApplications.hpp"
 
+#ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
+#endif
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
 #include <windows.h>
+#include <wtypes.h>
 #include <propkey.h>
 #include <propsys.h>
 #include <shellapi.h>
