e766bcbf220f6642e48a2c4e3f6e029361cfa34b
xt9y <felix@xt9y.de>
2026-09-09 23:56:54 +0200

    Handle AppBar position notifications

--- Source/Plasma/PanelBridge.hpp
+++ Source/Plasma/PanelBridge.hpp
@@ -1,12 +1,13 @@
 #pragma once
 
+#include <QAbstractNativeEventFilter>
 #include <QObject>
 
 class QWindow;
 
 namespace kde_windows
 {
-class PanelBridge final : public QObject
+class PanelBridge final : public QObject, public QAbstractNativeEventFilter
 {
     Q_OBJECT
 
@@ -15,12 +16,14 @@ public:
     ~PanelBridge() override;
 
     Q_INVOKABLE void registerPanel(QObject* object);
+    bool nativeEventFilter(const QByteArray& eventType, void* message, qintptr* result) override;
 
 private:
     void updatePosition();
     void unregisterPanel();
 
     QWindow* window_ = nullptr;
+    unsigned callbackMessage_ = 0;
     bool registered_ = false;
 };
 }
