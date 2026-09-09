8398e7479108e9675d4429570136f9809746f09a
xt9y <felix@xt9y.de>
2026-09-09 23:09:12 +0200

    Add native Wi-Fi controls

--- Source/Platform/Windows/WindowsNetworkSystem.hpp
+++ Source/Platform/Windows/WindowsNetworkSystem.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
+#include "Core/NetworkModel.hpp"
 #include "Core/SystemState.hpp"
+
+#include <string>
+#include <vector>
 
 namespace kde_windows
 {
@@ -8,5 +12,10 @@ class WindowsNetworkSystem final
 {
 public:
     [[nodiscard]] NetworkState state() const;
+    [[nodiscard]] std::vector<WifiNetworkSnapshot> networks() const;
+    [[nodiscard]] bool radioEnabled() const;
+    [[nodiscard]] bool setRadioEnabled(bool enabled) const;
+    [[nodiscard]] bool connectKnown(const std::wstring& networkId) const;
+    [[nodiscard]] bool disconnect() const;
 };
 }
