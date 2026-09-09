751e6b4df4e373bab91b06d3eff60b89de0864f3
xt9y <felix@xt9y.de>
2026-09-10 00:45:38 +0200

    Guard Win32 shortcut macros

--- Source/Platform/Windows/WindowsShortcutSystem.hpp
+++ Source/Platform/Windows/WindowsShortcutSystem.hpp
@@ -2,8 +2,12 @@
 
 #include "Core/ShortcutAction.hpp"
 
+#ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
+#endif
+#ifndef NOMINMAX
 #define NOMINMAX
+#endif
 #include <windows.h>
 
 #include <atomic>
