d16c47c963588b5239169aaa477e22a5f7d2096f
xt9y <felix@xt9y.de>
2026-09-09 21:59:55 +0200

    Initialize COM for native Plasma backends

--- Source/Plasma/main.cpp
+++ Source/Plasma/main.cpp
@@ -5,22 +5,37 @@
 #include <QQmlContext>
 #include <QQuickStyle>
 
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <windows.h>
+#include <objbase.h>
+
 int main(int argc, char** argv)
 {
-    QGuiApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
-    QGuiApplication app(argc, argv);
-    app.setApplicationName(QStringLiteral("plasmashell"));
-    app.setOrganizationName(QStringLiteral("KDE"));
-    QQuickStyle::setStyle(QStringLiteral("Basic"));
+    const HRESULT comResult = CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
+    const bool uninitializeCom = SUCCEEDED(comResult);
 
-    kde_windows::Backend backend;
-    QQmlApplicationEngine engine;
-    engine.rootContext()->setContextProperty(QStringLiteral("PlasmaBackend"), &backend);
+    int result = EXIT_FAILURE;
+    {
+        QGuiApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
+        QGuiApplication app(argc, argv);
+        app.setApplicationName(QStringLiteral("plasmashell"));
+        app.setOrganizationName(QStringLiteral("KDE"));
+        QQuickStyle::setStyle(QStringLiteral("Basic"));
 
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed, &app, [] {
-        QCoreApplication::exit(EXIT_FAILURE);
-    }, Qt::QueuedConnection);
+        kde_windows::Backend backend;
+        QQmlApplicationEngine engine;
+        engine.rootContext()->setContextProperty(QStringLiteral("PlasmaBackend"), &backend);
 
-    engine.loadFromModule(QStringLiteral("org.kde.plasma.windows"), QStringLiteral("Main"));
-    return app.exec();
+        QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed, &app, [] {
+            QCoreApplication::exit(EXIT_FAILURE);
+        }, Qt::QueuedConnection);
+
+        engine.loadFromModule(QStringLiteral("org.kde.plasma.windows"), QStringLiteral("Main"));
+        result = app.exec();
+    }
+
+    if (uninitializeCom)
+        CoUninitialize();
+    return result;
 }
