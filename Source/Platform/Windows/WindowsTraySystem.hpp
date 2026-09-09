5b8deab30b7b85884cafe38ec5ab0a8eae315795
xt9y <felix@xt9y.de>
2026-09-10 00:45:28 +0200

    Fix Win32 tray type declarations

--- Source/Platform/Windows/WindowsTraySystem.hpp
+++ Source/Platform/Windows/WindowsTraySystem.hpp
@@ -2,9 +2,14 @@
 
 #include "Core/TrayModel.hpp"
 
+#ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
+#endif
+#ifndef NOMINMAX
 #define NOMINMAX
+#endif
 #include <windows.h>
+#include <shellapi.h>
 
 #include <functional>
 #include <string>
