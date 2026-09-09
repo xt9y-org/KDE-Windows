4280f4e3de5a34790b03618a45b9c856657fe4d7
xt9y <felix@xt9y.de>
2026-09-10 01:51:56 +0200

    Keep MSVC include option attached in response files

--- Tools/build.ps1
+++ Tools/build.ps1
@@ -52,8 +52,7 @@ function Invoke-MSVC {
             '/DUNICODE',
             '/D_UNICODE',
             '/DNOMINMAX',
-            '/I',
-            '"Source"',
+            '/I"Source"',
             '/c',
             '"' + $source + '"',
             '/Fo"' + $object + '"'
