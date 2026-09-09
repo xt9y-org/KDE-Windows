a4afd3dc91649f48d2f8fc748738e4749a0ffcf9
xt9y <felix@xt9y.de>
2026-09-09 23:37:38 +0200

    Add Wi-Fi profile connection API

--- Source/Platform/Windows/WindowsNetworkSystem.hpp
+++ Source/Platform/Windows/WindowsNetworkSystem.hpp
@@ -16,6 +16,7 @@ public:
     [[nodiscard]] bool radioEnabled() const;
     [[nodiscard]] bool setRadioEnabled(bool enabled) const;
     [[nodiscard]] bool connectKnown(const std::wstring& networkId) const;
+    [[nodiscard]] bool connectWithPassword(const std::wstring& networkId, const std::wstring& password) const;
     [[nodiscard]] bool disconnect() const;
 };
 }
