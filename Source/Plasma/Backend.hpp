75c750df8bbfd9dc027ab51afb2386ce3c00e2bd
xt9y <felix@xt9y.de>
2026-09-09 22:16:48 +0200

    Expose Windows desktop and wallpaper to Plasma

--- Source/Plasma/Backend.hpp
+++ Source/Plasma/Backend.hpp
@@ -3,11 +3,13 @@
 #include "Core/ApplicationModel.hpp"
 #include "Core/AudioState.hpp"
 #include "Core/ClipboardModel.hpp"
+#include "Core/DesktopModel.hpp"
 #include "Core/SystemState.hpp"
 #include "Core/WindowModel.hpp"
 #include "Core/WindowSwitcher.hpp"
 #include "Platform/Windows/WindowsApplications.hpp"
 #include "Platform/Windows/WindowsAudioSystem.hpp"
+#include "Platform/Windows/WindowsDesktopSystem.hpp"
 #include "Platform/Windows/WindowsNetworkSystem.hpp"
 #include "Platform/Windows/WindowsPowerSystem.hpp"
 #include "Platform/Windows/WindowsShortcutSystem.hpp"
@@ -28,6 +30,8 @@ class Backend final : public QObject
     Q_OBJECT
     Q_PROPERTY(QVariantList windows READ windows NOTIFY windowsChanged)
     Q_PROPERTY(QVariantList applications READ applications NOTIFY applicationsChanged)
+    Q_PROPERTY(QVariantList desktopItems READ desktopItems NOTIFY desktopChanged)
+    Q_PROPERTY(QString wallpaperUrl READ wallpaperUrl NOTIFY desktopChanged)
     Q_PROPERTY(QVariantList trayIcons READ trayIcons NOTIFY trayIconsChanged)
     Q_PROPERTY(QVariantList notifications READ notifications NOTIFY notificationsChanged)
     Q_PROPERTY(QStringList clipboardEntries READ clipboardEntries NOTIFY clipboardChanged)
@@ -53,6 +57,8 @@ public:
 
     [[nodiscard]] QVariantList windows() const;
     [[nodiscard]] QVariantList applications() const;
+    [[nodiscard]] QVariantList desktopItems() const;
+    [[nodiscard]] QString wallpaperUrl() const;
     [[nodiscard]] QVariantList trayIcons() const;
     [[nodiscard]] QVariantList notifications() const { return notifications_; }
     [[nodiscard]] QStringList clipboardEntries() const;
@@ -80,6 +86,9 @@ public:
     Q_INVOKABLE void closeWindow(qulonglong id);
     Q_INVOKABLE void launchApplication(const QString& id);
     Q_INVOKABLE void reloadApplications();
+    Q_INVOKABLE void launchDesktopItem(const QString& id);
+    Q_INVOKABLE void reloadDesktop();
+    Q_INVOKABLE bool setWallpaper(const QString& path);
     Q_INVOKABLE void invokeTrayIcon(const QString& key, bool contextMenu = false);
     Q_INVOKABLE void dismissNotification(qulonglong id);
     Q_INVOKABLE void activateClipboardEntry(int index);
@@ -98,6 +107,7 @@ public:
 signals:
     void windowsChanged();
     void applicationsChanged();
+    void desktopChanged();
     void trayIconsChanged();
     void notificationsChanged();
     void clipboardChanged();
@@ -117,8 +127,10 @@ private:
     void commitWindowSwitcher();
     static QVariantMap windowMap(const WindowSnapshot& window);
     static QVariantMap applicationMap(const ApplicationEntry& application);
+    static QVariantMap desktopMap(const DesktopEntry& entry);
     static QVariantMap trayIconMap(const TrayIconSnapshot& icon);
     static QString trayIconImage(std::uintptr_t iconHandle);
+    static QString shellIconImage(const std::wstring& path);
     static bool enableShutdownPrivilege();
     static void exitWindows(unsigned flags);
     void reservePanel(QWindow* window);
@@ -126,9 +138,11 @@ private:
 
     WindowModel windowModel_;
     ApplicationModel applicationModel_;
+    DesktopModel desktopModel_;
     ClipboardModel clipboardModel_;
     WindowsWindowSystem windowSystem_;
     WindowsApplications applications_;
+    WindowsDesktopSystem desktopSystem_;
     WindowsTraySystem traySystem_;
     WindowsAudioSystem audioSystem_;
     WindowsPowerSystem powerSystem_;
@@ -139,7 +153,9 @@ private:
     NetworkState networkState_;
     QTimer clockTimer_;
     QTimer statusTimer_;
+    QTimer desktopTimer_;
     QVariantList notifications_;
+    std::wstring wallpaperPath_;
     qulonglong nextNotificationId_ = 1;
     std::size_t windowSwitcherSelection_ = kNoWindowSelection;
     bool windowSwitcherVisible_ = false;
