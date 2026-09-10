f6eeb07b13d774b45729429e3cb43a804d92122e
xt9y <felix@xt9y.de>
2026-09-10 11:51:13 +0200

    Use Breeze application palette on Windows

--- Source/Plasma/main.cpp
+++ Source/Plasma/main.cpp
@@ -3,10 +3,13 @@
 #include "PanelBridge.hpp"
 #include "ShellIconProvider.hpp"
 
+#include <QColor>
 #include <QCoreApplication>
 #include <QDir>
+#include <QFont>
 #include <QGuiApplication>
 #include <QIcon>
+#include <QPalette>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
 #include <QQuickStyle>
@@ -15,6 +18,30 @@
 #define NOMINMAX
 #include <windows.h>
 #include <objbase.h>
+
+namespace
+{
+QPalette breezeLightPalette()
+{
+    QPalette palette;
+    palette.setColor(QPalette::Window, QColor(QStringLiteral("#eff0f1")));
+    palette.setColor(QPalette::WindowText, QColor(QStringLiteral("#232629")));
+    palette.setColor(QPalette::Base, QColor(QStringLiteral("#fcfcfc")));
+    palette.setColor(QPalette::AlternateBase, QColor(QStringLiteral("#f5f6f7")));
+    palette.setColor(QPalette::ToolTipBase, QColor(QStringLiteral("#eff0f1")));
+    palette.setColor(QPalette::ToolTipText, QColor(QStringLiteral("#232629")));
+    palette.setColor(QPalette::Text, QColor(QStringLiteral("#232629")));
+    palette.setColor(QPalette::Button, QColor(QStringLiteral("#f2f3f4")));
+    palette.setColor(QPalette::ButtonText, QColor(QStringLiteral("#232629")));
+    palette.setColor(QPalette::BrightText, QColor(QStringLiteral("#ffffff")));
+    palette.setColor(QPalette::Highlight, QColor(QStringLiteral("#3daee9")));
+    palette.setColor(QPalette::HighlightedText, QColor(QStringLiteral("#ffffff")));
+    palette.setColor(QPalette::PlaceholderText, QColor(QStringLiteral("#7f8c8d")));
+    palette.setColor(QPalette::Disabled, QPalette::Text, QColor(QStringLiteral("#7f8c8d")));
+    palette.setColor(QPalette::Disabled, QPalette::ButtonText, QColor(QStringLiteral("#7f8c8d")));
+    return palette;
+}
+}
 
 int main(int argc, char** argv)
 {
@@ -27,6 +54,11 @@ int main(int argc, char** argv)
         QGuiApplication app(argc, argv);
         app.setApplicationName(QStringLiteral("plasmashell"));
         app.setOrganizationName(QStringLiteral("KDE"));
+        app.setPalette(breezeLightPalette());
+
+        QFont plasmaFont(QStringLiteral("Noto Sans"));
+        plasmaFont.setPointSize(10);
+        app.setFont(plasmaFont);
 
         const QDir binDir(QCoreApplication::applicationDirPath());
         const QString runtimeRoot = QDir::cleanPath(binDir.absoluteFilePath(QStringLiteral("..")));
